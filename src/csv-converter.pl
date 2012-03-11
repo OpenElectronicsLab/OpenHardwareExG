44d0a83899aed97b4289280c7af90bcf4d7f6a3a
Kendrick Shaw <kms15@case.edu>
2012-03-11 11:28:06 +0000

    cleaned up CSV converter

--- src/csv-converter.pl
+++ src/csv-converter.pl
@@ -5,19 +5,20 @@ use warnings;
 
 sub v {
     my $intval = hex(shift(@_));
-    my $ref = 4.;
-    my $potential = $intval * $ref / (2**23 - 1);
-    if ($intval > 0x7FFFFF) {
-        return $potential - 2 * $ref;
-    } else {
-        return $potential;
+    my $maxsigned24bit = 0x7FFFFF;
+    if ($intval > $maxsigned24bit) {
+        # two's complement
+        $intval = $intval - 2**24;
     }
+    my $refvoltage = 4.0; # volts
+    my $potential = $refvoltage * ($intval / (2.0**23 - 1));
+    return $potential;
 }
 
 my $regex =
 qr/\[go\]([0-9A-F]{6})([0-9A-F]{6})([0-9A-F]{6})([0-9A-F]{6})([0-9A-F]{6})([0-9A-F]{6})([0-9A-F]{6})([0-9A-F]{6})([0-9A-F]{6})\[on\]/;
 while (<>) {
     if ( $_ =~ m/$regex/ ) {
-        print $1 . "," . v($2) . "," . v($3) . "," . v($4) . "," . v($5) . "," . v($6) . "," . v($7) . "," . v($8) . "," . v($9) . "\n";
+        printf "%s,%11.7f,%11.7f,%11.7f,%11.7f,%11.7f,%11.7f,%11.7f,%11.7f\n", $1, v($2), v($3), v($4), v($5), v($6), v($7), v($8), v($9);
     }
 }
