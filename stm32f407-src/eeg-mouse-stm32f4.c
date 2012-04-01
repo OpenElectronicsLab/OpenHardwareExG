85104bd83c471855b3284909de1fa473aa278cd4
Kendrick Shaw <kms15@case.edu>
2012-04-01 01:51:24 +0000

    autoformat eeg-mouse-stm32f4.c

--- stm32f407-src/eeg-mouse-stm32f4.c
+++ stm32f407-src/eeg-mouse-stm32f4.c
@@ -24,20 +24,22 @@
 #include <libopencm3/usb/cdc.h>
 #include "eeg-mouse-usb-descriptors.h"
 
-static int cdcacm_control_request(struct usb_setup_data *req, u8 **buf,
-		u16 *len, void (**complete)(struct usb_setup_data *req))
+static int cdcacm_control_request(struct usb_setup_data *req, u8 ** buf,
+				  u16 * len,
+				  void (**complete) (struct usb_setup_data *
+						     req))
 {
 	(void)complete;
 	(void)buf;
 
 	switch (req->bRequest) {
-	case USB_CDC_REQ_SET_CONTROL_LINE_STATE: {
-		/*
-		 * This Linux cdc_acm driver requires this to be implemented
-		 * even though it's optional in the CDC spec, and we don't
-		 * advertise it in the ACM functional descriptor.
-		 */
-		return 1;
+	case USB_CDC_REQ_SET_CONTROL_LINE_STATE:{
+			/*
+			 * This Linux cdc_acm driver requires this to be implemented
+			 * even though it's optional in the CDC spec, and we don't
+			 * advertise it in the ACM functional descriptor.
+			 */
+			return 1;
 		}
 	case USB_CDC_REQ_SET_LINE_CODING:
 		if (*len < sizeof(struct usb_cdc_line_coding))
@@ -49,21 +51,21 @@ static int cdcacm_control_request(struct usb_setup_data *req, u8 **buf,
 }
 
 // rotates all of the letters in the buffer forward one letter.
-static void rotate_letters(char* buf, int len) {
-    int i;
+static void rotate_letters(char *buf, int len)
+{
+	int i;
 
-    for (i = 0; i < len; ++i) {
-        if (buf[i] >= 'a' && buf[i] < 'z') {
-            buf[i] = buf[i] + 1;
-        } else if (buf[i] == 'z') {
-            buf[i] = 'a';
-        }
-        else if (buf[i] >= 'A' && buf[i] < 'Z') {
-            buf[i] = buf[i] + 1;
-        } else if (buf[i] == 'Z') {
-            buf[i] = 'A';
-        }
-    }
+	for (i = 0; i < len; ++i) {
+		if (buf[i] >= 'a' && buf[i] < 'z') {
+			buf[i] = buf[i] + 1;
+		} else if (buf[i] == 'z') {
+			buf[i] = 'a';
+		} else if (buf[i] >= 'A' && buf[i] < 'Z') {
+			buf[i] = buf[i] + 1;
+		} else if (buf[i] == 'Z') {
+			buf[i] = 'A';
+		}
+	}
 }
 
 static void cdcacm_data_rx_cb(u8 ep)
@@ -74,14 +76,12 @@ static void cdcacm_data_rx_cb(u8 ep)
 	int len = usbd_ep_read_packet(0x01, buf, 64);
 
 	if (len) {
-        rotate_letters(buf, len);
-        while (usbd_ep_write_packet(0x82, buf, len) == 0)
-			;
+		rotate_letters(buf, len);
+		while (usbd_ep_write_packet(0x82, buf, len) == 0) ;
 	}
-
-    // flash the LEDs so we know we're doing something
+	// flash the LEDs so we know we're doing something
 	gpio_toggle(GPIOC, GPIO5);
-    gpio_toggle(GPIOD, GPIO12 | GPIO13 | GPIO14 | GPIO15);
+	gpio_toggle(GPIOD, GPIO12 | GPIO13 | GPIO14 | GPIO15);
 }
 
 static void cdcacm_set_config(u16 wValue)
@@ -92,10 +92,11 @@ static void cdcacm_set_config(u16 wValue)
 	usbd_ep_setup(0x82, USB_ENDPOINT_ATTR_BULK, 64, NULL);
 	usbd_ep_setup(0x83, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
 
-	usbd_register_control_callback(
-				USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE,
-				USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
-				cdcacm_control_request);
+	usbd_register_control_callback(USB_REQ_TYPE_CLASS |
+				       USB_REQ_TYPE_INTERFACE,
+				       USB_REQ_TYPE_TYPE |
+				       USB_REQ_TYPE_RECIPIENT,
+				       cdcacm_control_request);
 }
 
 int main(void)
@@ -107,7 +108,7 @@ int main(void)
 	rcc_peripheral_enable_clock(&RCC_AHB2ENR, RCC_AHB2ENR_OTGFSEN);
 	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPDEN);
 
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, 
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE,
 			GPIO9 | GPIO11 | GPIO12);
 	gpio_set_af(GPIOA, GPIO_AF10, GPIO9 | GPIO11 | GPIO12);
 
@@ -122,4 +123,3 @@ int main(void)
 	while (1)
 		usbd_poll();
 }
-
