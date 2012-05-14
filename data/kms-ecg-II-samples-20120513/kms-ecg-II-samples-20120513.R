70b0757bae5970672f9359830e786029b886b335
Kendrick Shaw <kms15@case.edu>
2012-05-14 00:28:00 +0000

    simple ecg data

---
+++ data/kms-ecg-II-samples-20120513/kms-ecg-II-samples-20120513.R
@@ -0,0 +1,31 @@
+#!/usr/bin/RScript
+
+# lead II
+II <- read.csv("kms-ecg-II-samples-20120513.csv")[,1];
+
+# sample rate (samples per second)
+rate <- 250
+
+# num samples
+N <- length(II);
+
+# times
+t <- (0:(N - 1)) / rate;
+
+# fft frequencies
+f <- (0:(N - 1)) / N * rate;
+
+# fft samples
+II_fft <- fft(II)
+
+# band pass filter the result
+highPass <- 1; # Hz
+highPassWidth <- 0.1; # Hz
+lowPass <- 40; # Hz
+lowPassWidth <- 1; # Hz
+# windowing function (just the product of two sigmoid functions)
+win <- function(freq) { 1 / ((1 + exp(-(freq - highPass) / highPassWidth)) * (1 + exp((freq - lowPass) / lowPassWidth))) };
+II_fft_f <- II_fft * (win(f) + win((1 - 1/N) * rate - f));
+II_f <- Re(fft(II_fft_f, inverse=TRUE) / N);
+
+plot(t, II_f, type="l", lwd=2, xlim=c(0,5))
