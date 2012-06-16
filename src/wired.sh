5623562163ca18e4828b1933c69c02ddea50f276
Eric Herman <eric@freesa.org>
2012-06-16 12:48:05 +0200

    save data before filtering

--- src/wired.sh
+++ src/wired.sh
@@ -4,7 +4,7 @@ SAMPLE=`date +"%Y%m%d.%H%M%S"`
 
 ./serial-reader.pl |
 ./frame-parser.pl |
-./parsed-frame-filter.pl |
 tee -a sample-${USER}-${SAMPLE}.csv |
+./parsed-frame-filter.pl |
 ./window.pl |
 ./plot.pl
