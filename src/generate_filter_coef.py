12a38ed80aa8d54a6d8ce3ad18c86f32059c72e7
Eric Herman <eric@freesa.org>
2012-06-23 16:58:54 +0200

    add graphing to coefficients generation

--- src/generate_filter_coef.py
+++ src/generate_filter_coef.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python
+import numpy as np
 from scipy import signal
+from matplotlib import pyplot as plt
+
+nyquest_freq = 250./2;
 
 # 10-12 Hz 10th order elliptic bandpass filter, from
-x_filter = signal.iirdesign(wp = [10./125, 12./125], ws= [7./125, 17./125],
-     gstop= 110, gpass=1, ftype='ellip')
+x_filter = signal.iirdesign(
+    wp = [10./nyquest_freq, 12./nyquest_freq],
+    ws = [7./nyquest_freq, 17./nyquest_freq],
+    gstop=110, gpass=1, ftype='ellip'
+)
 
 # 21.5-24.5 Hz 12th order elliptic bandpass filter, from
-y_filter = signal.iirdesign(wp=[21.5/125, 24.5/125], ws=[18./125, 29./125],
-     gstop=105, gpass=1, ftype='ellip')
+y_filter = signal.iirdesign(
+    wp = [21.5/nyquest_freq, 24.5/nyquest_freq],
+    ws = [18./nyquest_freq, 29./nyquest_freq],
+    gstop=105, gpass=1, ftype='ellip'
+)
 
 # 5 Hz 10th order elliptic lowpass filter, from
-smooth_filter = signal.iirdesign(wp = 5./125, ws= 7./125, gstop=100,
-     gpass=1, ftype='ellip')
+smooth_filter = signal.iirdesign(
+    wp = 5./nyquest_freq,
+    ws= 7./nyquest_freq, gstop=100,
+    gpass=1, ftype='ellip'
+)
+
+
+fig = plt.figure()
+for filter in [x_filter, y_filter, smooth_filter]:
+    w,h = signal.freqz(*filter)
+    plt.plot(w*(nyquest_freq/max(w)), np.abs(h))
+    plt.xlim(0,40)
+    plt.xlabel('frequency (Hz)');
+    plt.ylabel('response');
+fig.savefig("test.png")
 
 with open('filter_coefs.yaml','wt') as f:
     f.write('x_filter:\n');
