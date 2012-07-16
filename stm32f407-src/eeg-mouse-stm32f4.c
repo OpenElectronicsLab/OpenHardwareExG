2181d69f57dcb24c05cb9902583325a254383148
Eric Herman <eric@freesa.org>
2012-07-16 19:30:36 +0200

    sketch out eeg-mouse logic

--- stm32f407-src/eeg-mouse-stm32f4.c
+++ stm32f407-src/eeg-mouse-stm32f4.c
@@ -28,24 +28,27 @@
 #include "eeg-mouse-usb-descriptors.h"
 #include "util.h"
 #include "opencm3util.h"
+// actual size today is 64, but a few extra will not hurt
+#define DATA_BUF_SIZE 80
+// GLOBALS
+int send_data;
+// END GLOBALS
 
 static int cdcacm_control_request(struct usb_setup_data *req, u8 ** buf,
 				  u16 * len,
 				  void (**complete) (struct usb_setup_data *
 						     req))
 {
-	/* by casting to void, we avoid an unused argument warning */
+	// by casting to void, we avoid an unused argument warning
 	(void)complete;
 	(void)buf;
 
 	switch (req->bRequest) {
 	case USB_CDC_REQ_SET_CONTROL_LINE_STATE:{
-			/*
-			 * This Linux cdc_acm driver requires this to be
-			 * implemented even though it's optional in the CDC
-			 * spec, and we don't advertise it in the ACM
-			 * functional descriptor.
-			 */
+			// This Linux cdc_acm driver requires this to be
+			// implemented even though it's optional in the CDC
+			// spec, and we don't advertise it in the ACM
+			// functional descriptor.
 			return 1;
 		}
 	case USB_CDC_REQ_SET_LINE_CODING:
@@ -71,131 +74,23 @@ u8 send_command(u16 command, u8 data)
 	return (u8) return_value;
 }
 
-u8 read_motion_axis(u8 axis)
-{
-	u16 command;
-	u8 data;
-
-	data = 0;
-
-	command =
-	    /* READ bit */
-	    (0x1 << 7) |
-	    /* MS bit:  When 0 do not increment address */
-	    (0x0 << 6) |
-	    /* bits 2-7 are address */
-	    (axis << 0);
-
-	return send_command(command, data);
-}
-
-u32 read_motion()
-{
-	u8 x, y, z;
-	u32 combined;
-
-	x = read_motion_axis(0x29);
-	y = read_motion_axis(0x2B);
-	z = read_motion_axis(0x2D);
-
-	combined = (((u32) x) << 16) | (((u32) y) << 8) | z;
-	return combined;
-}
-
-void setup_accelerometer()
-{
-	u16 command;
-	u8 data;
-
-	command =
-	    /* READ bit not set */
-	    (0x0 << 7) |
-	    /* MS bit:  When 0 do not increment address */
-	    (0x0 << 6) |
-	    /* bits 2-7 are address */
-	    (0x20 << 0);
-
-	data =
-	    /* data rate selection, 1 = 400Hz */
-	    (0x1 << 7) |
-	    /* power down control, 1 = active */
-	    (0x1 << 6) |
-	    /* full scale selection (1 = 8G, 0 = 2G) */
-	    (0x0 << 5) |
-	    /* Z axis enable */
-	    (0x1 << 2) |
-	    /* Y axis enable */
-	    (0x1 << 1) |
-	    /* X axis enable */
-	    (0x1 << 0);
-
-	send_command(command, data);
-}
-
-// rotates all of the letters in the buffer forward one letter.
-static void echo_with_read_motion(char *buf, int *len)
-{
-	int i;
-	u32 motion;
-	u8 x, y, z;
-
-	motion = read_motion();
-	x = (u8) (motion >> 16);
-	y = (u8) (motion >> 8);
-	z = (u8) (motion >> 0);
-
-	i = 0;
-
-	buf[i++] = 'X';
-	buf[i++] = ':';
-	buf[i++] = ' ';
-	buf[i++] = '0';
-	buf[i++] = 'x';
-	buf[i++] = to_hex(x, 1);
-	buf[i++] = to_hex(x, 0);
-
-	buf[i++] = ',';
-	buf[i++] = ' ';
-
-	buf[i++] = 'Y';
-	buf[i++] = ':';
-	buf[i++] = ' ';
-	buf[i++] = '0';
-	buf[i++] = 'x';
-	buf[i++] = to_hex(y, 1);
-	buf[i++] = to_hex(y, 0);
-
-	buf[i++] = ',';
-	buf[i++] = ' ';
-
-	buf[i++] = 'Z';
-	buf[i++] = ':';
-	buf[i++] = ' ';
-	buf[i++] = '0';
-	buf[i++] = 'x';
-	buf[i++] = to_hex(z, 1);
-	buf[i++] = to_hex(z, 0);
-
-	buf[i++] = '\r';
-	buf[i++] = '\n';
-
-	*len = i;
-}
-
 static void cdcacm_data_rx_cb(u8 ep)
 {
-	(void)ep;
 
 	char buf[64];
 	int len = usbd_ep_read_packet(0x01, buf, 64);
 
+	// by casting to void, we avoid an unused argument warning
+	(void)ep;
+
+	// if (ep != EEG_MOUSE_USB_DATA_ENDPOINT) {
+	//      return;
+	// }
+
 	if (len) {
-		echo_with_read_motion(buf, &len);
-		while (usbd_ep_write_packet(EEG_MOUSE_USB_DATA_ENDPOINT, buf,
-			len) == 0) {
-		}
+		send_data = buf[0];
 	}
-	/* flash the LEDs so we know we're doing something */
+	// flash the LEDs so we know we're doing something
 	gpio_toggle(GPIOD, GPIO12 | GPIO13 | GPIO14 | GPIO15);
 }
 
@@ -204,7 +99,8 @@ static void cdcacm_set_config(u16 wValue)
 	(void)wValue;
 
 	usbd_ep_setup(0x01, USB_ENDPOINT_ATTR_BULK, 64, cdcacm_data_rx_cb);
-	usbd_ep_setup(EEG_MOUSE_USB_DATA_ENDPOINT, USB_ENDPOINT_ATTR_BULK, 64, NULL);
+	usbd_ep_setup(EEG_MOUSE_USB_DATA_ENDPOINT, USB_ENDPOINT_ATTR_BULK, 64,
+		      NULL);
 	usbd_ep_setup(0x83, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
 
 	usbd_register_control_callback(USB_REQ_TYPE_CLASS |
@@ -222,19 +118,19 @@ void setup_main_clock()
 void setup_peripheral_clocks()
 {
 	rcc_peripheral_enable_clock(&RCC_AHB1ENR,
-				    /* GPIO A */
+				    // GPIO A
 				    RCC_AHB1ENR_IOPAEN |
-				    /* GPIO D */
+				    // GPIO D
 				    RCC_AHB1ENR_IOPDEN |
-				    /* GPIO E */
+				    // GPIO E
 				    RCC_AHB1ENR_IOPEEN);
 
 	rcc_peripheral_enable_clock(&RCC_AHB2ENR,
-				    /* USB OTG */
+				    // USB OTG
 				    RCC_AHB2ENR_OTGFSEN);
 
 	rcc_peripheral_enable_clock(&RCC_APB2ENR,
-				    /* SPI 1 */
+				    // SPI 1
 				    RCC_APB2ENR_SPI1EN);
 }
 
@@ -251,23 +147,23 @@ void setup_usb_fullspeed()
 void setup_spi()
 {
 	gpio_mode_setup(SPI_GPIO, GPIO_MODE_AF, GPIO_PUPD_NONE,
-			/* serial clock */
+			// serial clock
 			PIN_SCLK |
-			/* master in/slave out */
+			// master in/slave out
 			PIN_DIN |
-			/* master out/slave in */
+			// master out/slave in
 			PIN_DOUT);
 	gpio_set_af(SPI_GPIO, GPIO_AF5, PIN_SCLK | PIN_DIN | PIN_DOUT);
 
 	spi_disable_crc(SPI1);
 	spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_32,
-			/* high or low for the peripheral device */
+			// high or low for the peripheral device
 			SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
-			/* CPHA: Clock phase: read on falling edge of clock */
+			// CPHA: Clock phase: read on falling edge of clock
 			SPI_CR1_CPHA_CLK_TRANSITION_2,
-			/* DFF: Date frame format (8 or 16 bit) */
+			// DFF: Date frame format (8 or 16 bit)
 			SPI_CR1_DFF_8BIT,
-			/* Most or Least Sig Bit First */
+			// Most or Least Sig Bit First
 			SPI_CR1_MSBFIRST);
 
 	spi_enable_software_slave_management(SPI1);
@@ -278,17 +174,28 @@ void setup_spi()
 	spi_enable(SPI1);
 }
 
-void print_error(const char *buf, u16 len)
+void print_msg(const char *msg, u16 len)
 {
-	while (usbd_ep_write_packet(EEG_MOUSE_USB_DATA_ENDPOINT, buf,
-		len) == 0) {
+	while (usbd_ep_write_packet(EEG_MOUSE_USB_DATA_ENDPOINT, msg, len) == 0) {
 	}
+}
+
+void print_error(const char *msg, u16 len)
+{
+      print_msg("[oh]", 4);
+      print_msg(msg, len);
+      print_msg("[no]\r\n", 6);
+}
+
+int data_ready()
+{
+	return 0 == gpio_get(ADS_GPIO, IPIN_DRDY);
 }
 
 void wait_for_drdy(const char *msg, u16 msg_len, unsigned interval)
 {
 	unsigned i = 0;
-	while (gpio_get(ADS_GPIO, IPIN_DRDY) != 0) {
+	while (!data_ready()) {
 		usbd_poll();
 		if (i < interval) {
 			continue;
@@ -322,13 +229,10 @@ void setup_ads1298()
 {
 	unsigned i;
 
-	/* chip select */
+	// chip select
 	gpio_mode_setup(ADS_GPIO, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
 			IPIN_CS |
-			PIN_CLKSEL |
-			IPIN_PWDN |
-			IPIN_RESET |
-			PIN_START);
+			PIN_CLKSEL | IPIN_PWDN | IPIN_RESET | PIN_START);
 
 	gpio_mode_setup(ADS_GPIO, GPIO_MODE_INPUT, GPIO_PUPD_NONE, IPIN_DRDY);
 
@@ -363,7 +267,8 @@ void setup_ads1298()
 	adc_wreg(GPIO, 0);
 
 	// Power up the internal reference and wait for it to settle
-	adc_wreg(CONFIG3, RLDREF_INT | PD_RLD | PD_REFBUF | VREF_4V | CONFIG3_const);
+	adc_wreg(CONFIG3,
+		 RLDREF_INT | PD_RLD | PD_REFBUF | VREF_4V | CONFIG3_const);
 	pause_microseconds(150 * 1000);
 
 	adc_wreg(RLD_SENSP, 0x01);	// only use channel IN1P and IN1N
@@ -383,28 +288,69 @@ void setup_ads1298()
 	}
 
 	gpio_set(ADS_GPIO, PIN_START);
-	wait_for_drdy("waiting for DRDY in setup\r\n", 26, 1000);
+	wait_for_drdy("waiting for DRDY in setup", 25, 1000);
 }
 
 void setup_leds()
 {
-	/* enable the four LEDs */
-	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT,
-			GPIO_PUPD_NONE, GPIO12 | GPIO13 | GPIO14 | GPIO15);
-	/* Set two LEDs for wigwag effect when toggling. */
+	// enable the four LEDs
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
+			GPIO12 | GPIO13 | GPIO14 | GPIO15);
+	// Set two LEDs for wigwag effect when toggling.
 	gpio_set(GPIOD, GPIO12 | GPIO14);
+}
+
+// if this becomes more flexible, we may need to pass in
+// the byte_buf size, but for now we are safe to skip it
+void fill_sample_frame(char *byte_buf)
+{
+	int i, j;
+	char in_byte;
+
+	unsigned int pos = 0;
+
+	gpio_clear(ADS_GPIO, IPIN_CS);
+	byte_buf[pos++] = '[';
+	byte_buf[pos++] = 'g';
+	byte_buf[pos++] = 'o';
+	byte_buf[pos++] = ']';
+
+	// read 24bits of status then 24bits for each channel
+	for (i = 0; i <= 8; ++i) {
+		for (j = 0; j < 3; ++j) {
+			in_byte = spi_xfer(SPI1, 0);
+			byte_buf[pos++] = to_hex(in_byte, 1);
+			byte_buf[pos++] = to_hex(in_byte, 0);
+		}
+	}
+
+	byte_buf[pos++] = '[';
+	byte_buf[pos++] = 'o';
+	byte_buf[pos++] = 'n';
+	byte_buf[pos++] = ']';
+	byte_buf[pos++] = '\r';
+	byte_buf[pos++] = '\n';
+	byte_buf[pos++] = 0;
+	pause_microseconds(1);
+	gpio_set(ADS_GPIO, IPIN_CS);
 }
 
 int main(void)
 {
+	char byte_buf[DATA_BUF_SIZE];
+
 	setup_main_clock();
 	setup_peripheral_clocks();
 	setup_ads1298();
 	setup_usb_fullspeed();
 	setup_leds();
-	setup_accelerometer();
 
 	while (1) {
-		usbd_poll();
+		// wait_for_drdy calls usbd_poll()
+		wait_for_drdy("no data", 7, 5000);
+		fill_sample_frame(byte_buf);
+		if (send_data) {
+			print_msg(byte_buf, DATA_BUF_SIZE);
+		}
 	}
 }
