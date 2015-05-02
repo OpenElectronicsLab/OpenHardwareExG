5b24a7221ed1b59ca44919158b4a64a9714382e1
Eric Herman <eric@freesa.org>
2015-05-02 16:30:30 +0200

    add supplier to bom-maker.pl

--- kicad-files/rev1/bom-maker.pl
+++ kicad-files/rev1/bom-maker.pl
@@ -16,27 +16,29 @@ sub by_number {
 while (<>) {
     my $line = $_;
     chomp $line;
-    my ( $ref, $value, $footprint, $partnum ) = split ',', $line;
+    my ( $ref, $value, $footprint, $partnum, $supplier ) = split ',', $line;
     next if ( !$partnum );
-    $parts->{$partnum}->{value} = $value;
-    $parts->{$partnum}->{refs} ||= [];
-    push @{ $parts->{$partnum}->{refs} }, $ref;
-    @{ $parts->{$partnum}->{refs} } =
-      sort by_number @{ $parts->{$partnum}->{refs} };
-    $parts->{$partnum}->{count}++;
+    $parts->{$supplier}->{$partnum}->{value} = $value;
+    $parts->{$supplier}->{$partnum}->{refs} ||= [];
+    push @{ $parts->{$supplier}->{$partnum}->{refs} }, $ref;
+    @{ $parts->{$supplier}->{$partnum}->{refs} } =
+      sort by_number @{ $parts->{$supplier}->{$partnum}->{refs} };
+    $parts->{$supplier}->{$partnum}->{count}++;
 }
 
-for my $partnum (
-    sort {
-        substr( $parts->{$a}->{refs}->[0], 0, 1 ) cmp
-          substr( $parts->{$b}->{refs}->[0], 0, 1 )
-          or int( substr( $parts->{$a}->{refs}->[0], 1 ) ) <=>
-          int( substr( $parts->{$b}->{refs}->[0], 1 ) )
-    } keys %$parts
-  )
-{
-    my $part = $parts->{$partnum};
-    my $cust_num = join( "_", @{ $part->{refs} } );
-    print $partnum, "  |  ", $part->{count}, "  |  ", $part->{value}, "  |  ",
-      $cust_num, "\n";
+for my $supplier ( keys %$parts ) {
+    for my $partnum (
+        sort {
+            substr( $parts->{$supplier}->{$a}->{refs}->[0], 0, 1 ) cmp
+              substr( $parts->{$supplier}->{$b}->{refs}->[0], 0, 1 )
+              or int( substr( $parts->{$supplier}->{$a}->{refs}->[0], 1 ) ) <=>
+              int( substr( $parts->{$supplier}->{$b}->{refs}->[0], 1 ) )
+        } keys %{ $parts->{$supplier} }
+      )
+    {
+        my $part = $parts->{$supplier}->{$partnum};
+        my $cust_num = join( "_", @{ $part->{refs} } );
+        print $supplier, "  |   ", $partnum, "  |  ", $part->{count}, "  |  ",
+          $part->{value}, "  |  ", $cust_num, "\n";
+    }
 }
